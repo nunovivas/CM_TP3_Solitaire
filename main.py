b4c57b107dca1ee9affa32fa1f5a09e66ce45a47
Nuno Vivas <nuno.vivas@gmail.com>
2024-04-02 23:48:01 +0100

    bugged but last version

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
+import logging
+
 import flet as ft
+from layout import create_appbar
+from settings import Settings
 from solitaire import Solitaire
- 
+
+# logging.basicConfig(level=logging.DEBUG)
+
+
 def main(page: ft.Page):
-  
-   solitaire = Solitaire()
- 
-   page.add(solitaire)
- 
-ft.app(target=main)
+    def on_new_game(settings):
+        page.controls.pop()
+        new_solitaire = Solitaire(settings, on_win)
+        page.add(new_solitaire)
+        page.update()
+
+    def on_win():
+        page.add(ft.AlertDialog(title=ft.Text("YOU WIN!"), open=True, on_dismiss=lambda e: page.controls.pop()))
+        print("You win")
+        page.update()
+
+    settings = Settings()
+    create_appbar(page, settings, on_new_game)
+
+    solitaire = Solitaire(settings, on_win)
+    page.add(solitaire)
+
+
+#ft.app(target=main, assets_dir="assets")
+ft.app(target=main,assets_dir="assets", port=8080, view=ft.WEB_BROWSER)
+
--- slot.py
+++ slot.py
@@ -1,15 +1,39 @@
-SLOT_WIDTH = 70
-SLOT_HEIGHT = 100
- 
 import flet as ft
- 
+
 class Slot(ft.Container):
-    def __init__(self, top, left):
+    def __init__(self, solitaire, slot_type, top, left, border):
         super().__init__()
-        self.pile=[]
-        self.width=SLOT_WIDTH
-        self.height=SLOT_HEIGHT
-        self.left=left
-        self.top=top
-        self.border=ft.border.all(1)
-        self.bgcolor=ft.colors.WHITE # by Nuno
+        self.solitaire = solitaire
+        self.pile = []
+        self.type = slot_type
+        self.width = 70
+        self.height = 100
+        self.left = left
+        self.top = top
+        self.border_radius = ft.border_radius.all(6)
+        self.border = border
+        self.on_click = self.click
+
+    def get_top_card(self):
+        if len(self.pile) > 0:
+            return self.pile[-1]
+
+    def get_top_three_cards(self):
+        n = len(self.pile)
+        return self.pile[max(0, n-3):]
+
+    def fan_top_three(self):
+        for i, card in enumerate(self.get_top_three_cards()):
+            card.left = self.left + self.solitaire.card_offset * i
+            card.visible = True
+
+    def upper_card_top(self):
+        if self.type == "tableau":
+            if len(self.pile) > 1:
+                return self.top + self.solitaire.card_offset * (len(self.pile) - 1)
+        return self.top
+
+    def click(self, e):
+        if self.type == "stock" and self.solitaire.deck_passes_remaining > 1:
+            self.solitaire.deck_passes_remaining -= 1
+            self.solitaire.restart_stock()
